2a1d1f3f905045953ff331d31b01d1910bfc199d
marinasavluchinska <anauk2018@gmail.com>
2019-03-07 09:22:24 +0200

    chat work

--- src/main/java/tinder/com/impl/MessagesDAO_SQl.java
+++ src/main/java/tinder/com/impl/MessagesDAO_SQl.java
@@ -95,9 +95,9 @@ public class MessagesDAO_SQl implements DAO<MessageItem> {
         int count = 0;
         try {
             Statement st = conn.createStatement();
-            ResultSet resultSet = st.executeQuery("select count(*) from messages");
+            ResultSet resultSet = st.executeQuery("select count(*) as count from messages");
             while (resultSet.next()) {
-                count = resultSet.getInt("count(*");
+                count = resultSet.getInt("count");
             }
             resultSet.close();
 
@@ -106,25 +106,25 @@ public class MessagesDAO_SQl implements DAO<MessageItem> {
         }
         return count == 0;
     }
-    public List<MessageItemExtra> getByUser(int id1, int id2){
-        ArrayList<MessageItemExtra> messeges = new ArrayList<>();
+    public List<MessageItem> getByUser(int id1, int id2){
+        ArrayList<MessageItem> messeges = new ArrayList<>();
         try{
-            PreparedStatement prs = conn.prepareStatement("select * from messages AS msg " +
-                    "join users AS user1 on msg.id1 = user1.id" +
-                    "join users AS user2 on msg.id2 = user2.id" +
-                    "where (id1=? AND id2=?) " +
-                    "or (id1=? AND id2=?) order by ASC ");
+            PreparedStatement prs = conn.prepareStatement("select * from messages AS msgs join users AS us1 on msgs.id1 = us1.id join users AS us2 on msgs.id2 = us2.id where (id1=? AND id2=?) or (id1=? AND id2=?) order by time ASC");
+            prs.setInt(1, id1);
+            prs.setInt(2, id2);
+            prs.setInt(3, id2);
+            prs.setInt(4, id1);
             ResultSet resultSet = prs.executeQuery();
             while(resultSet.next()){
-                MessageItemExtra item = new MessageItemExtra(
+                MessageItem item = new MessageItem(
                         resultSet.getInt("id"),
                         resultSet.getInt("id1"),
                         resultSet.getInt("id2"),
                         resultSet.getString("messages"),
                         resultSet.getTimestamp("time"),
-                        resultSet.getString("user1.name"),
-                        resultSet.getString("user2.name"),
-                        resultSet.getString("user1.photo")
+                        resultSet.getString("name"),
+                        resultSet.getString("name"),
+                        resultSet.getString("photo")
                 );
                 messeges.add(item);
             }
